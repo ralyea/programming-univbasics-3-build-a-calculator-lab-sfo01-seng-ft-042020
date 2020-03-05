d0298d1e303b363764c0680919d9401f36e8f518
Paul Ralyea <047par@gmail.com>
2020-03-05 22:24:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,25 @@ def addition(num1, num2)
 end
   
 def subtraction(num1, num2)
-  puts num1 - num2
+  x = num1 - num2
+  return x
 end
   
 def division(num1, num2)
-  puts num1 / num2
+  x = num1 / num2
+  return x
 end
   
 def multiplication(num1, num2)
-  puts num1 * num2
+  x = num1 * num2
+  return x
 end
   
 def modulo(num1, num2)
-  puts num1 % num2
+  x = num1 % num2
+  return x
 end
   
 def square_root(num)
-  puts Math.sqrt(num)
+  x = Math.sqrt(num)
 end
